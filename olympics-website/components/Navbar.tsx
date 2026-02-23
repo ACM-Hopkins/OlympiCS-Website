9ae77ed9081c234a395efbb70d4bbc52cd6a34ed
seth-wyzy <swyzyko1@jh.edu>
2026-02-23 17:02:27 -0500

    added pathnames to navbar

--- olympics-website/components/Navbar.tsx
+++ olympics-website/components/Navbar.tsx
@@ -1,14 +1,34 @@
 //Home, Rules, Schedule, Scoreboard, Teams, Register
-import Link from 'next/link';
 
+"use client";
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+    const pathname = usePathname();
     return (
-    <nav className = "w-full bg-white shadow-md px-6 py-5 flex items-center justify-between">
-        <Link href="/" className="text -xl font-Geist text-gray-800">Hopkins OlympiCS</Link>
-        <div className = "flex gap-6">
-            <Link href="/" className="text-blue-600 hover:text-black transition">Home</Link>
-            <Link href="/about" className = "text-blue-600 hover:text-black transition">About</Link>
+    <nav className = "w-full bg-white shadow-md px-6 py-5 flex items-center justify-left">
+        <Link href="/" className="text -xl font-bold text-gray-800">Hopkins OlympiCS</Link>
+        <div className = "flex gap-10">
+            <Link href="/" className={pathname === "/" ? "text-black font-semibold" : "text-gray-500 hover:text-black"}>
+                Home
+            </Link>
+            <Link href='/rules' className= {pathname === '/rules' ? "text-black font-semibold" : "text-gray-500 hover:text-black"}>
+                Rules
+            </Link>
+            <Link href='/schedule' className = {pathname === '/schedule' ? "text-black font-semibold" : "text-gray-500 hover:text-black"}>
+                Schedule
+            </Link>
+            <Link href='/scoreboard' className = {pathname === '/scoreboard' ? "text-black font-semibold" : "text-gray-500 hover:text-black"}>
+                Scoreboard
+            </Link>
+            <Link href='/teams' className = {pathname === '/teams' ? "text-black font-semibold" : "text-gray-500 hover:text-black"}>
+            Teams
+            </Link>
+            <Link href='/register' className = {pathname === '/register' ? "text-black font-semibold" : "text-gray-500 hover:text-black"}>
+            Register
+            </Link>
         </div>
     </nav>
     );
