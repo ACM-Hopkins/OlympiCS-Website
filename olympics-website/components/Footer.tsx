d9ff70ea61374b6f57e8dee20f897834a9ba1c58
seth-wyzy <swyzyko1@jh.edu>
2026-02-23 10:39:16 -0500

    added preliminary working navbar and footer framework:

---
+++ olympics-website/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+    <footer className = "w-full bg-gray-100 text-center text-gray-500 py-6 mt-auto">
+        <p> FIXME DUHHH</p>
+    </footer>
+    );
+}
--- olympics-website/components/Navbar.tsx
+++ olympics-website/components/Navbar.tsx
@@ -1,1 +1,15 @@
 //Home, Rules, Schedule, Scoreboard, Teams, Register
+import Link from 'next/link';
+
+
+export default function Navbar() {
+    return (
+    <nav className = "w-full bg-white shadow-md px-6 py-5 flex items-center justify-between">
+        <Link href="/" className="text -xl font-Geist text-gray-800">Hopkins OlympiCS</Link>
+        <div className = "flex gap-6">
+            <Link href="/" className="text-blue-600 hover:text-black transition">Home</Link>
+            <Link href="/about" className = "text-blue-600 hover:text-black transition">About</Link>
+        </div>
+    </nav>
+    );
+}
