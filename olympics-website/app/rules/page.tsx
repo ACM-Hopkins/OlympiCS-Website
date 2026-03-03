1e38d25ff6f586c875de1998d27b5b1b4990ddea
seth-wyzy <swyzyko1@jh.edu>
2026-03-02 19:43:27 -0500

    Navbar and Footer Blank Implementation
    * added components folder w/ navbar and footer file
    
    * added preliminary working navbar and footer framework:
    
    * added pathnames to navbar
    
    * started hambuger
    
    * hamburger working
    
    * basic functionality working
    
    * deleted massive comment
    
    * added pages.tsx

---
+++ olympics-website/app/rules/page.tsx
@@ -0,0 +1,7 @@
+export default function rulesPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Rules</h1>
+    </div>
+  );
+}
